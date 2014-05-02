bb91553cb2e3165e4e899208c36b5b299bc3edc0
torotero <ganza.toro@gmail.com>
2014-05-02 14:16:36 -0400

    memisahkan form di file terpisah dan merendernya

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,8 +34,20 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def edit
+		@article =Article.find(params[:id])
+	end
 
+	def update
+		@article =Article.find(params[:id])
 
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+		
+	end
 
 
 	private
